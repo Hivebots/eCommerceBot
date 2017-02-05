8eb910080399905433ab18ccefc8db5fae7c1bbb
Amy Kate Nicholson <amykate_7@live.com>
2017-02-05 13:58:18 -0800

    added fuzzy search and loop back

--- dialogs/productSearch.js
+++ dialogs/productSearch.js
@@ -1,14 +1,15 @@
 module.exports = function () {
     bot.dialog('/productSearch', [
+        
         function (session) {
             //Prompt for string input
             builder.Prompts.text(session, "Type in the name of the product you are searching for:");
         },
-        function (session, results) {
+        function (session, results, next) {
             //Sets name equal to resulting input
             var name = results.response;
 
-            var queryString = searchQueryStringBuilder('search= ' + name, "product");
+            var queryString = searchQueryStringBuilder('search=' + name + '~1', "product");
             performSearchQuery(queryString, function (err, result) {
                 if (err) {
                     console.log("Error when searching for product: " + err);
@@ -17,8 +18,30 @@ module.exports = function () {
                     session.replaceDialog('/showResults', { result });
                 } else {
                     session.endDialog("No products by the name \'" + name + "\' found");
+                    builder.Prompts.confirm(session, "Would you like to search again?", { listStyle: builder.ListStyle.button });
+                    next();
+
                 }
             })
+        },
+        function (session, results) {
+        if (results.response) {
+            var selection = results.response.entity;
+            // route to corresponding dialogs
+            switch (selection) {
+                case "Yes":
+                    session.replaceDialog('/productSearch');
+                    break;
+                case "No":
+                    session.send("Taking you back to home");
+                    session.replaceDialog('/promptButtons');
+                    break;
+                default:
+                    session.reset('/');
+                    break;
+            }
         }
-    ]);
+    }
+
+]);
 }
