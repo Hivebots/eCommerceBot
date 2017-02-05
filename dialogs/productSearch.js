0359eb7b5142c40bc9f7f516e385a97ae1928def
Lilian Kasem <liliankasem@gmail.com>
2017-02-05 11:18:55 -0800

    Set up plain search scenario

--- dialogs/productSearch.js
+++ dialogs/productSearch.js
@@ -2,21 +2,21 @@ module.exports = function () {
     bot.dialog('/productSearch', [
         function (session) {
             //Prompt for string input
-            builder.Prompts.text(session, "Type in the name of the musician you are searching for:");
+            builder.Prompts.text(session, "Type in the name of the product you are searching for:");
         },
         function (session, results) {
             //Sets name equal to resulting input
             var name = results.response;
 
-            var queryString = searchQueryStringBuilder('search= ' + name);
+            var queryString = searchQueryStringBuilder('search= ' + name, "product");
             performSearchQuery(queryString, function (err, result) {
                 if (err) {
-                    console.log("Error when searching for musician: " + err);
+                    console.log("Error when searching for product: " + err);
                 } else if (result && result['value'] && result['value'][0]) {
                     //If we have results send them to the showResults dialog (acts like a decoupled view)
                     session.replaceDialog('/showResults', { result });
                 } else {
-                    session.endDialog("No musicians by the name \'" + name + "\' found");
+                    session.endDialog("No products by the name \'" + name + "\' found");
                 }
             })
         }
